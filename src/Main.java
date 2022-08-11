75e6b492af596b777f1022e6492dce25f8b69ea3
Lenovo <nadyasafarova98@gmail.com>
2022-08-11 18:49:08 +0300

    ДЗ от Нади 4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+           //Задача1
+        int i=1;
+        while (i<=10){
+            System.out.print(i+" ");
+            i++;
+
+        }
+        System.out.println( );
+        for(int i1=10;i1>0;i1--){
+            System.out.print(i1+" ");
+        }
+         // Задача 2
+        for (int a=1;a<=31;a=a+7){
+            System.out.println("Сегодня пятница, "+a+" -е число. Необходимо подготовить отчет");
+        }
+            //Задача 3
+        int start=1822;
+        for (int year=1822;year<=2122;year++){
+            if(year %79==0){
+            System.out.println(year);
+        }
+        }
+
+
     }
 }
